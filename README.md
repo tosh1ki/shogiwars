93096689d46a6c52d59c2aa4663e55dd8e3f0744
tosh1ki <tosh1ki@yahoo.co.jp>
2014-10-25 15:25:36 +0900

    mongodbを追加など

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 とりあえず棋譜収集の機能を作っていく
 
-## 参考文献
+### 将棋ウォーズのrobots.txt
+棋譜をクローリングするわけなので，一応robots.txtを確認しておこう，と思って見てみたら全文コメントアウトされていた．そのうち編集される可能性が高いので，プログラムを使用する際には注意．
+
+[shogiwars.heroz.jp/robots.txt](http://shogiwars.heroz.jp/robots.txt)
+
+### 参考文献
 
 - [CSA標準棋譜ファイル形式](http://www.computer-shogi.org/protocol/record_v22.html)
--- wars-an.py
+++ wars-an.py
@@ -2,22 +2,25 @@
 # -*- coding: utf-8 -*-
 
 ##--------------------------------------------------------------##
+## Filename: wars-an.py
 ## Author  : tosh1ki
 ## E-mail  : tosh1ki@yahoo.co.jp
-## Outline : 将棋ウォーズの棋譜取得プログラム いずれは解析プログラムへ
+## Outline : 将棋ウォーズの棋譜取得,DB登録プログラム
 ## Since   : 2014-10-24
 ##--------------------------------------------------------------##
 
-
+import sys
 import urllib2
 from BeautifulSoup import BeautifulSoup
-import time
-import pdb
 import re
-from datetime import datetime as dt
+import time
+import datetime as dt
+import pymongo
+
 
 INTERVAL_TIME = 5
-SWARS_URL_PATTERN = re.compile(r'(?:http\:\/\/shogiwars\.heroz\.jp\:3002\/games\/)(?P<sente>\w+)-(?P<gote>\w+)-(?P<date>[0-9_]+)')
+WCSA_PATTERN = re.compile(r'(?<=receiveMove\(\").+(?=\"\);)')
+GAME_HEADER_PATTERN = re.compile(r'(?<=var\sgamedata\s=\s){.+?}', re.DOTALL)
 
 
 def get_html(url):
@@ -29,26 +32,28 @@ def get_html(url):
 
     return html
 
-def get_url_list(user, gtype='', start=1):
+def get_url_list(user, gtype='', start=1, max_iter=10):
     u''' 指定したユーザーの棋譜を取得する．
 
     Parameters
     ----------
-    user : User name (string)
-    gtype : Kifu type (string) 
-        ''   : 10 min. (default)
-        'sb' :  3 min. (bullet mode)
-        's1' : 10 sec.
-    start : Number of start point (Numeric)
+    user
+        User name (string)
+    gtype
+        Kifu type (string)
+        example
+            ''   : 10 min. (default)
+            'sb' :  3 min. (bullet mode)
+            's1' : 10 sec.
+    start
+        Number of start point (Numeric)
+    max_iter
+        取得する最大数．10個ずつ取得するたびに判定しているので10個弱はずれる．
 
     Example
     ----------
     >>> username = '1_tsutsukana'
     >>> url_list = get_url_list(username)
-    11
-    21
-    31
-    41
     >>> len(url_list)>0
     True
     '''
@@ -72,10 +77,12 @@ def get_url_list(user, gtype='', start=1):
 
         url_list.extend(url_list_tmp)
         start += 10
-        print start
-        break ## とりあえず1ループで止めておく
         
+        if start > max_iter:
+            break 
+
     return url_list
+
 
 def wcsa_to_csa(wars_csa, gtype):
     u''' 将棋ウォーズ専用?のCSA形式を一般のCSA形式に変換する．
@@ -90,14 +97,13 @@ def wcsa_to_csa(wars_csa, gtype):
 
     wcsa_list = re.split(r'[,\t]', wars_csa)
 
-    if gtype == '':
+    if gtype == '': 
         max_time = 60*10
-    elif gtype == 'sb':
+    elif gtype == 'sb': 
         max_time = 60*3
-    elif gtype == 's1':
-        ## TODO: 10秒切れ負けの場合を実装
-        return 0
-    else :
+    elif gtype == 's1': 
+        max_time = 3600
+    else: 
         print 'Error: gtypeに不正な値; gtype={0}'.format(gtype)
 
     sente_prev_remain_time = max_time
@@ -108,17 +114,22 @@ def wcsa_to_csa(wars_csa, gtype):
     for i,w in enumerate(wcsa_list):
         if i%2==0:
             ## 駒の動き，あるいは特殊な命令の処理
-            ## TODO: GOTE_TORYO_hoge みたいな文字列を
-            ##       きちんとCSA形式のものに置換する．
-            results.append(w)
-        else:
+            ## CAUTION: 仕様がわからないので全部網羅できているかわからない
+            if w.find('TORYO') > 0:
+                w_ap = '%TORYO'
+            elif w.find('TIMEOUT') > 0:
+                w_ap = '%TIME_UP'
+            else:
+                w_ap = w
 
+            results.append(w_ap)
+
+        else:
             if (i-1)%4==0:
                 ## 先手の残り時間を計算
                 sente_remain_time = int(w[1:])
                 _time = sente_prev_remain_time - sente_remain_time
                 sente_prev_remain_time = sente_remain_time
-
             else: 
                 ## 後手の残り時間を計算
                 gote_remain_time = int(w[1:])
@@ -127,39 +138,79 @@ def wcsa_to_csa(wars_csa, gtype):
 
             results.append('T' + str(_time))
 
-
     return '\n'.join(results)
 
 
 def url_to_kifudata(url):
     u''' urlが指す棋譜とそれに関する情報を辞書にまとめて返す．
 
-    明示されていない棋譜ページの仕様に依存しているので，そのうち使えなくなる可能性がある．
+    CAUTION : 明示されていない棋譜ページの仕様に依存しているので，
+    そのうち使えなくなる可能性がある．
     '''
     html = get_html(url)
-
+    
     ## 対局に関するデータの取得
-    res = re.findall(r'(?<=var\sgamedata\s=\s){.+?}', html, re.DOTALL)[0]
+    res = re.findall(GAME_HEADER_PATTERN, html)[0]
     _dict = eval(re.sub(r'\n\t\t(?P<key>\w+)(?=:)','"\g<key>"', res))
     _dict['user0'], _dict['user1'], _dict['date'] = _dict['name'].split('-')
 
     ## 棋譜の取得
-    wars_csa = re.findall(r'(?<=receiveMove\(\").+(?=\"\);)', html)[0]
-    _dict['csa'] = wcsa_to_csa(wars_csa, '')
+    wars_csa = re.findall(WCSA_PATTERN, html)[0]
+    _dict['csa'] = wcsa_to_csa(wars_csa, _dict['gtype'])
 
     ## 時刻オブジェクトの生成
-    _dict['datetime'] = dt.strptime(date, '%Y%m%d_%H%M%S')
+    _dict['datetime'] = dt.datetime.strptime(_dict['date'], '%Y%m%d_%H%M%S')
+
+    _dict['_id'] = _dict.pop('name')
 
     return _dict
 
 
+def connect_mongodb():
+    u''' MongoDBに接続
+    '''
+    connection = pymongo.Connection('localhost',27017)
+    db = connection.warskifu
+    return db.kifu
+
+
+def append_mongodb(url_list, reflesh=False):
+    u''' url_list 中の url の指す棋譜を取得してCSA形式に変換，mongoDBに追加．
+    '''
+
+    id_pattern = re.compile(r'\w+-\w+-\w+')
+
+    ## connect to mongoDB
+    col = connect_mongodb()
+
+    n_list = len(url_list)
+    sec = n_list * INTERVAL_TIME
+    finish_time = dt.datetime.now() + dt.timedelta(seconds=sec)
+
+    print '{0}件の棋譜'.format(n_list)
+    print '棋譜収集終了予定時刻 : {0}'.format(finish_time)
+    sys.stdout.flush()
+
+    for _url in url_list:
+        _id = re.findall(id_pattern, _url)[0]
+
+        ## DB 内にあって，かつrefleshがfalseのときはcontinueする
+        if col.find({u'_id':_id}).count() > 0 and not reflesh:
+            continue
+
+        kifu_dict = url_to_kifudata(_url)
+        col.insert(kifu_dict)
+
 
 if __name__ == '__main__':
 
+    ## Example
+    username = '1_tsutsukana'
+    gtype = 's1'
+    max_iter = 10000
+
     ## 棋譜のurlのリストを取得
-    url_list = get_url_list('1_tsutsukana', gtype='')
+    url_list = get_url_list(username, gtype=gtype, max_iter=max_iter)
 
-    ## test code
-    ui = 0
-    url_to_kifudata(url_list[ui])
-
+    ## mongoDB に追加
+    append_mongodb(url_list)
